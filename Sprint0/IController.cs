f697d3d4cae35f7c3cee08d99995ed70cc7d1070
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-09-17 18:25:13 -0400

    Initial Commit

---
+++ Sprint0/IController.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public interface IController {
+	void Update();
+}
---
+++ Sprint0/TextSprite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+public class TextSprite : ISprite {
+
+    private int totalFrames;
+    private int currentFrame;
+    private SpriteFont font;
+
+    private List<Rectangle> textRects;
+
+    public TextSprite(int x, int y, SpriteFont sf) {
+        textRects = new List<Rectangle>();
+        font = sf;
+        currentFrame = 0;
+        totalFrames = textRects.Count;
+    }
+
+    void ISprite.Update(int x, int y) {
+        currentFrame++;
+        if(currentFrame >= totalFrames) {
+            currentFrame = 0;
+        }
+    }
+
+    void ISprite.Draw(SpriteBatch sb, Texture2D txt) {
+        sb.DrawString(font, "Credits: ", new Vector2(200, 220), Color.Purple);
+        sb.DrawString(font, "Program Made By: Shivam Engineer", new Vector2(200, 250), Color.Purple);
+        sb.DrawString(font, "Sprites from: Files Section of Carmen Canvas Page", new Vector2(200, 280), Color.Purple);
+    }
+}
