438a925080135ba3921c9a0fcdc928c4433a5e3f
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-09-20 23:34:18 -0400

    Revert "Added Some Enemy Interfaces"
    This reverts commit b38dcc3108e097de154a4fb0d39c16dd0f67ce06.

--- Sprint0/IEnemy.cs
+++
@@ -1,6 +0,0 @@
-namespace Sprint0 {
-    public interface IEnemy {
-        void update();
-        void takeDamage();
-    }
-}
--- Sprint0/IEnemyState.cs
+++
@@ -1,6 +0,0 @@
-namespace Sprint0 {
-    public interface IEnemyState {
-        void changeDirection();
-        void Update();
-    }
-}
