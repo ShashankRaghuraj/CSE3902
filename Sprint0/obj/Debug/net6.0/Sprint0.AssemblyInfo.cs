b7a1ac995f5f719bf83c4fff0ef2cdc1a30e0034
ShashankRaghuraj <shashankraghuraj12@gmail.com>
2024-09-22 18:51:15 -0400

    Revert "Link Spritesheet Uploaded"
    This reverts commit 7be52c090887573fe4b0fa4ee871c4aa163894f5.

--- Sprint0/obj/Debug/net6.0/Sprint0.AssemblyInfo.cs
+++ Sprint0/obj/Debug/net6.0/Sprint0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2577281d1ffdaeb6ee7589efe3cf15b668591769")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00aeb3466e66a55ce11e256fc3236d42acb760a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
